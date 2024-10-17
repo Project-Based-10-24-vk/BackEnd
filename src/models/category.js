54c5d29a458f1263749157b8b5a5eddd308faeb8
zim89 <121241841+zim89@users.noreply.github.com>
2024-10-17 14:36:49 +0300

    add Category model
    - add property CATEGORY to consts/models
    - add property COLOR_PATTERN to regex in consts/validation
    - add Category model

---
+++ src/models/category.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose')
+const { CATEGORY } = require('~/consts/models')
+const { FIELD_CANNOT_BE_EMPTY } = require('~/consts/errors')
+const { regex } = require('~/consts/validation')
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, FIELD_CANNOT_BE_EMPTY('name')],
+      unique: true,
+      lowercase: true
+    },
+    appearance: {
+      icon: {
+        type: String,
+        required: [true, FIELD_CANNOT_BE_EMPTY('icon')],
+        default: 'mocked-path-to-icon'
+      },
+      color: {
+        type: String,
+        required: [true, FIELD_CANNOT_BE_EMPTY('color')],
+        default: '#66C42C',
+        lowercase: true,
+        validate: {
+          validator: function (color) {
+            return regex.COLOR_PATTERN.test(color)
+          },
+          message: 'Color must be in hex format'
+        }
+      }
+    }
+  },
+  { timestamps: true, versionKey: false }
+)
+
+module.exports = model(CATEGORY, categorySchema)
