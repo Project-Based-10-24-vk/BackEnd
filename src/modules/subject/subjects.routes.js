ea93c370d245ef22c010950b2a7e26827996a3d2
AlexApiko <Alex@apiko.com>
2024-10-26 14:53:43 +0300

    add routes description

--- src/modules/subject/subjects.routes.js
+++ src/modules/subject/subjects.routes.js
@@ -11,13 +11,35 @@ const {
 router.use(authMiddleware)
 router.param('id', idValidation)
 
+// @desc    Get all subjects
+// @route 	GET /subject
+// @access  Private (Authenticated users)
 router.get('/', asyncWrapper(subjectController.subjectsFind))
+
+// @desc    Get subject by id
+// @route 	GET /subject/:id
+// @access  Private (Authenticated users)
 router.get('/:id', asyncWrapper(subjectController.subjectFindById))
+
+// @desc    Get all subject by categories id
+// @route 	GET /subject/categories/:id/subjects
+// @access  Private (Authenticated users)
 router.get('/categories/:id/subjects', asyncWrapper(subjectController.subjectsFindByCategoryId))
 
 router.use(restrictTo(ADMIN))
+// @desc    Create subject
+// @route 	POST /subject
+// @access  Private (ADMIN)
 router.post('/', asyncWrapper(subjectController.subjectCreate))
+
+// @desc    Update subject
+// @route 	PUT /subject
+// @access  Private (ADMIN)
 router.put('/:id', asyncWrapper(subjectController.subjectUpdate))
+
+// @desc    Delete subject
+// @route 	DELETE /subject
+// @access  Private (ADMIN)
 router.delete('/:id', asyncWrapper(subjectController.subjectDelete))
 
 module.exports = router
