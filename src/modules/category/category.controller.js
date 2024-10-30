0439c6b9db333b8c72d0a5c7b3f320f945e618f5
AlexApiko <Alex@apiko.com>
2024-10-30 19:00:45 +0200

    update docs and add sort option

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -1,5 +1,6 @@
 const getMatchOptions = require('~/utils/getMatchOptions')
 const getRegex = require('~/utils/getRegex')
+const getSortOptions = require('~/utils/getSortOptions')
 
 const categoryService = require('./category.service')
 
@@ -25,7 +26,8 @@ const findOneById = async (req, res) => {
 const subjectsFindByCategoryId = async (req, res) => {
   const { id } = req.params
   const { sort = '', skip, limit } = req.query
-  const response = await categoryService.getSubjectsByCategoryId(id, sort, skip, limit)
+  const sortOptions = getSortOptions(sort)
+  const response = await categoryService.getSubjectsByCategoryId(id, sortOptions, skip, limit)
 
   res.status(200).json(response)
 }
