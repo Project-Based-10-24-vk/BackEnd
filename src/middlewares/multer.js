d14ddce5af847c9e892caff7303bf6f99785f170
nadraharr <nadraharr@gmail.com>
2024-11-06 12:26:39 +0200

    extension validation

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -6,6 +6,15 @@ const upload = multer({
   limits: {
     fileSize: 10 * 1024 * 1024,
     fieldNameSize: 100
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png|pdf/
+    const extension = file.mimetype.split('/')[1]
+    if (allowedTypes.test(extension)) {
+      cb(null, true)
+    } else {
+      cb(new Error('Invalid file type. Only JPEG, PNG, and PDF files are allowed.'))
+    }
   }
 })
 
