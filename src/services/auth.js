f2181baa1c5e751cd787190b887887f244ca6814
Victor Voitenko <102621431+VoitV@users.noreply.github.com>
2024-11-08 13:42:49 +0200

    extend jwt payload token for user login (#95)
    * extend jwt token for user login
    
    * update refresh token payload
    
    ---------
    
    Co-authored-by: AlexApiko <Alex@apiko.com>

--- src/services/auth.js
+++ src/services/auth.js
@@ -43,13 +43,14 @@ const authService = {
       throw createError(401, INCORRECT_CREDENTIALS)
     }
 
-    const { _id, lastLoginAs, isFirstLogin, isEmailConfirmed } = user
+    //In future add another data
+    const { _id, lastLoginAs, isFirstLogin, isEmailConfirmed, firstName, lastName } = user
 
     if (!isEmailConfirmed) {
       throw createError(401, EMAIL_NOT_CONFIRMED)
     }
 
-    const tokens = tokenService.generateTokens({ id: _id, role: lastLoginAs, isFirstLogin })
+    const tokens = tokenService.generateTokens({ id: _id, role: lastLoginAs, isFirstLogin, firstName, lastName })
     await tokenService.saveToken(_id, tokens.refreshToken, REFRESH_TOKEN)
 
     if (isFirstLogin) {
@@ -73,9 +74,10 @@ const authService = {
       throw createError(400, BAD_REFRESH_TOKEN)
     }
 
-    const { _id, lastLoginAs, isFirstLogin } = await getUserById(tokenData.id)
-
-    const tokens = tokenService.generateTokens({ id: _id, role: lastLoginAs, isFirstLogin })
+    const { _id, lastLoginAs, isFirstLogin, firstName, lastName } = await getUserById(tokenData.id)
+    
+    //In future add another data
+    const tokens = tokenService.generateTokens({ id: _id, role: lastLoginAs, isFirstLogin, firstName, lastName })
     await tokenService.saveToken(_id, tokens.refreshToken, REFRESH_TOKEN)
 
     return tokens
